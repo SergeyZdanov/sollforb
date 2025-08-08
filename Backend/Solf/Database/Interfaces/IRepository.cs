6ed7debf5e8b6fcfc2ad8c5ae52c98436cea288b
Сергей <sergey>
2025-08-08 20:34:31 +0300

    fix1

--- Backend/Solf/Database/Interfaces/IRepository.cs
+++ Backend/Solf/Database/Interfaces/IRepository.cs
@@ -2,6 +2,10 @@
 {
     public interface IRepository<T>
     {
+        Task<T> CreateAsync(T entity);
+        Task<T> GetByIdAsync(int id);
         Task<List<T>> GetAllAsync();
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
     }
 }
